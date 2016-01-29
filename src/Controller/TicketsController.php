1196b155832161deb77eac90abef45fa7044906c
sirisha <sirisha.g@osmosys.asia>
2016-01-29 11:27:24 +0530

    --> wrote method to update the ticket information.

--- src/Controller/TicketsController.php
+++ src/Controller/TicketsController.php
@@ -8,6 +8,9 @@ use Cake\Mailer\Email;
 
 class TicketsController extends AppController{
     
+    /**
+     * Gets list of all active tickets in database
+     */
     public function index(){
         $active = 1;
         $tickets = $this->Tickets->find('all', 
@@ -16,11 +19,18 @@ class TicketsController extends AppController{
         echo json_encode($tickets);
     }
     
+    /**
+     * Get the respective ticket data
+     * @param type $id
+     */
     public function view($id){
         $ticketDetails = $this->Tickets->get($id);
         echo $ticketDetails;
     }
     
+    /**
+     * For inserting ticket data
+     */
     public function add(){
         $ticketTable = TableRegistry::get('tickets');
         $ticket = $ticketTable->newEntity();
@@ -38,7 +48,7 @@ class TicketsController extends AppController{
                     if($ticketTable->save($ticket)){
                         $this->setAction('sendMail');
                         $response['status'] = 'success';
-                        $response['data'] = 'There was an error while inserting data.';
+                        $response['data'] = 'Request inserted successfully.';
                     }else{
                         $response['status'] = 'error';
                         $response['data'] = 'There was an error while inserting data.';
@@ -51,6 +61,46 @@ class TicketsController extends AppController{
         }
     }
     
+    /**
+     * Edit the ticket information
+     * @param type $id
+     */
+    public function edit($id = null){
+        $ticketTable = TableRegistry::get('tickets');
+        $ticket = $ticketTable->get($id, [
+            'contain' => []
+        ]);
+        if($this->request->is(['put', 'patch'])){
+            $input = $this->request->data;
+            if(!empty($input)){
+                $response = array();
+                $ticket = $ticketTable->patchEntity($ticket, $input);
+                if($ticket->errors()){
+                    $response['status'] = 'error';
+                    $response['data'] = $ticket->errors();
+                }else{
+                    $ticket['modified_on'] = date('Y-m-d H:i:s');
+                    if($ticketTable->save($ticket)){
+                          //To Do  
+//                        $this->setAction('sendMail');
+                        
+                        $response['status'] = 'success';
+                        $response['data'] = 'Request updated successfully.';
+                    }else{
+                        $response['status'] = 'error';
+                        $response['data'] = 'There was an error while updating data.';
+                    }
+                }
+                echo json_encode($response);
+            }else{
+                echo 'Input should not be empty.';
+            }
+        }
+    }
+    
+    /**
+     * For sending mail with specific infomation
+     */
     public function sendMail(){
         $input = $this->request->data;
         $subject = $input['subject'];
